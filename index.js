93d7d380626b91bb943004f6273a09212ef7c9e1
Akaki <akakikoridze@yahoo.com>
2020-02-16 09:44:05 -0500

    updated and completed

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
 $(function() {
 
-    // fetch('https://raw.githubusercontent.com/StrategicFS/Recruitment/master/data.json')
-    //     .then(res => res.json())
-    //     .then(data => {
-    //         console.log(data);
-    //     })
-    //     .catch(err => {
-    //         console.log(err);
+    // Fetching Elements. We could use both of this methods either with jQuery or Fetch API
+
+    // $.getJSON("https://raw.githubusercontent.com/StrategicFS/Recruitment/master/data.json",  (data) => {
+    // });
+
+
+    fetch('https://raw.githubusercontent.com/StrategicFS/Recruitment/master/data.json')
+        .then(res => res.json())
+        .then(data => {
+            let i = 0
+            while (i < data.length) {
+                renderTr(data[i])
+
+                i++
+            }
+
+            addCheckListen()
+        })
+        .catch(err => {
+            console.log(err);
             
-    //     })
+    })
+
+
+    // Defining variables
 
     var sum = 0
     var totalRow = 0
     var checkRow = 0
+
+
+    // To render tableraws. This is used for initial fetch method as well as when new item is added to the table
 
     function renderTr(obj) {
         let item = []
@@ -24,22 +43,14 @@ $(function() {
         item.push("<td style='text-align: right'>" + obj.balance + ".00 </td>");
 
         $("<tr/>", { html: item.join("") }).appendTo(".mainTable")
+
+        totalRow++
+        rowCount(totalRow)
+        
     }
 
-    
-    $.getJSON("https://raw.githubusercontent.com/StrategicFS/Recruitment/master/data.json",  (data) => {
-        
-        let i = 0 
-        while (i < data.length) {
-            renderTr(data[i])
 
-            
-            i++
-        }
-        totalRow += data.length
-        rowCount(totalRow)
-    });
-
+    // toggling clicks on add button. First click adds input areas. Second click adds filled input area values to the table
 
     var clickCount = 0
     $('#add').on('click', () => {
@@ -59,6 +70,8 @@ $(function() {
         clickCount++
     }
     
+    // when new item is added to the table we re-render the table
+    // listener is also added to checkbox input dinamicly
 
     else  {
     
@@ -69,17 +82,18 @@ $(function() {
         })
 
         $('.inputText').remove()
-        renderTr(obj)   
+        renderTr(obj) 
 
-        totalRow ++
-        rowCount(totalRow)
-
-        
         clickCount++
         } 
-    })
+
+        addCheckListen()
         
+    })
+    
                   
+// Looking for checked checkboxes and removing its row in the table
+// Decrementing total row count by checked element number
 
     $('#remove').on('click', () => {
         totalRow -= $(".check:checked").length
@@ -88,63 +102,85 @@ $(function() {
     })
     
 
+// checking all the checkboxes 
+// re rendering checked boxes count and summing all the values for total sum
+
+
     $('#markAll').on('change', () => {
 
         if ($('#markAll').prop("checked") === true) {
             $(".check").prop("checked", true)
+            checkRow = totalRow
+            checkCount(checkRow)
+
+
+            $(".check").each((i, el) => {
+                sum += Number($(el).parent().parent().children().last().text())
+            })
+            total(sum)
+
         }
+
+// unchecking all checkboxes and re rendering checked boxes count and sum to 0
         else {
             $(".check").prop("checked", false)
+            checkRow = 0
+            checkCount(checkRow)
+
+            sum = 0
+            total(0)
         }
         
     })
 
 
+// adding change listeners to checkboxes dynamicly while rendering the table 
+// in order to avoid two change listeners previous listener is unbinded to all emenets
+// check row count is incremented and sum is increased by the added element value
+
+
+    function addCheckListen() {
+        $(".check").unbind()
+        $(".check").each((i, el) => {
+            $(el).on('change', () => {
+                if ($(el).prop("checked") === true) {
+                    checkRow++
+                    checkCount(checkRow);
+                    
+                    sum += Number($(el).parent().parent().children().last().text()) 
+                    total(sum)
+                }
+
+// decrementing checkrow and sum by removed item value
+                else {
+                    checkRow--
+                    checkCount(checkRow);
+
+                    sum -= Number($(el).parent().parent().children().last().text()) 
+                    total(sum)
+                }
+            })
+        })
+
+    }
+
+// helper functions to render page and its elements of total row count checked checkboxs count and total sum
 
     function rowCount(num) {
         $('#rowCount').children().last().remove()
         $(`<div>${num}</div>`).appendTo('#rowCount')
     }
 
-
     function checkCount(num) {
         $('#checkCount').children().last().remove()
         $(`<div>${num}</div>`).appendTo('#checkCount')
     }
-
-
-    // $(".check").prop("checked").length
-    // checkCount(checkRow)
-
-    // checkCount(checkRow)
     
-
-console.log($(".check"));
-
-
-    // $("input:checkbox").each((i, el) => {
-    //     console.log('hi');
-        
-    //     $(el).on('change', () => {
-    //         console.log($(el));
- 
-
-    //     })
-    // })
-    
-    
-
-    
-
-    function total() {
-        $(".check:checked").parent().parent().last().each( (i, element)=>{
-
-            sum += element.val()
-            console.log(sum);
-        })
-        
-        
+    function total(num) {
+        $('.totalWrapper').children().last().remove()
+        $(`<div>$${num}</div>`).appendTo('.totalWrapper')
     }
+
 
 
 
